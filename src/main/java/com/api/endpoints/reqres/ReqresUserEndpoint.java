64ce52c425e07fc32177b399424697e01c84f9b1
Nazarii Kupchenko <nazarii.kupchenko@gmail.com>
2022-02-22 15:41:14 +0200

    TR-82 add methods for single user endpoint

--- src/main/java/com/api/endpoints/reqres/ReqresUserEndpoint.java
+++ src/main/java/com/api/endpoints/reqres/ReqresUserEndpoint.java
@@ -4,6 +4,8 @@ import static com.api.utils.Properties.REQRES_BASE_URI;
 import static com.api.utils.Properties.REQRES_USERS_END_POINT;
 
 import com.api.endpoints.Api;
+import com.api.models.reqres.CreateUserModel;
+import com.api.models.reqres.UpdateUserModel;
 import io.restassured.response.ValidatableResponse;
 
 public class ReqresUserEndpoint extends Api {
@@ -13,5 +15,26 @@ public class ReqresUserEndpoint extends Api {
         .get(REQRES_USERS_END_POINT + id)
         .then()
         .statusCode(200);
+  }
+
+  public ValidatableResponse createSingleUser(CreateUserModel body) {
+    return requestSpecification(REQRES_BASE_URI)
+        .post(REQRES_USERS_END_POINT + body)
+        .then()
+        .statusCode(201);
+  }
+
+  public ValidatableResponse updateSingleUser(UpdateUserModel body) {
+    return requestSpecification((REQRES_BASE_URI))
+        .put(REQRES_USERS_END_POINT + body)
+        .then()
+        .statusCode(200);
+  }
+
+  public ValidatableResponse deleteSingleUser(int id) {
+    return requestSpecification(REQRES_BASE_URI)
+        .delete(REQRES_USERS_END_POINT + id)
+        .then()
+        .statusCode(204);
   }
 }
